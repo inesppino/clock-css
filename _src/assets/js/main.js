3b997bc33074b3514832dbe3670989bd9f19042f
Inés Pedraza Pino <inesppino@mbp-de-ines.dhcp.mano.es.sopra>
2019-08-06 11:08:49 +0200

    feat: clock

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-console.log('>> Ready :)');
+const secondHand = document.querySelector('.second-hand');
+const hourHand = document.querySelector('.hour-hand');
+const minHand = document.querySelector('.min-hand');
+
+function setDate(){
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secDegrees = ((seconds/60) * 360 + 90);
+  secondHand.style.transform = `rotate(${secDegrees}deg)`;
+
+  const hour = now.getHours();
+  const hourDegree = ((hour/12) * 360 + 90);
+  hourHand.style.transform = `rotate(${hourDegree}deg)`;
+
+  const mins = now.getMinutes();
+  const minDegree = ((mins/60) * 360 + 90);
+  minHand.style.transform = `rotate(${minDegree}deg)`;
+
+  if(secDegrees === 90 || hourDegree === 90 || minDegree === 90) {
+    secondHand.style.transition = "0s";
+    hourHand.style.transition = "0s";
+    minHand.style.transition = "0s";
+  } else {
+    secondHand.style.transition = "all 0.5s";
+    hourHand.style.transition = "all 0.5s";
+    minHand.style.transition = "all 0.5s";
+  }
+}
+
+setInterval(setDate, 1000);
